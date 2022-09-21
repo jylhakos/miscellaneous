9e6f9780ba05babf91b0cd8781441c3981e584dd
laptop <juha.jylhakoski@aalto.fi>
2022-09-21 12:00:29 +0800

    Python

---
+++ QUESTIONS/PYTHON/Arrays/arrays.py
@@ -0,0 +1,30 @@
+# $ python3 arrays.py
+
+import numpy as np
+
+def longest(s):
+    maxLen = 1
+    if s == '':
+        return 0
+    for i in range(len(s)):
+        substring = s[i]
+        for j in range(i+1, len(s)):
+            if s[j] not in substring:
+                substring = substring + s[j]
+                maxLen = max(maxLen, len(substring))
+            else:
+                break
+    return maxLen
+
+def nearest(array, value):
+    array = np.asarray(array)
+    idx = (np.abs(array - value)).argmin()
+    return array[idx]
+
+array = np.array([-4, -2, 1, 4, 8])
+
+print(nearest(array, value=0))
+
+s = "abcabcbb"
+
+print(longest(s))
